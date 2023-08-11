645f874fee17e825a00ff8245896d5db0f7b759f
gj94 <gokuljayaraj@icloud.com>
2023-08-12 00:37:30 +0800

    First commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@RestController
+	class Controller{
+
+
+		@GetMapping("/")
+		public String sayhello(){
+			return "Hello";
+		}
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
